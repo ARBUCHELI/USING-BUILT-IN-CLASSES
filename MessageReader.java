fe3188acb585d694f6ee86d69593b1c83d933ee5
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-27 13:28:24 +0300

    MessageReader.java

---
+++ MessageReader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MessageReader {
+	public static void main(String[] args) throws IOException {
+		File file = new File("./message.txt");
+		
+    Scanner scan = new Scanner(file);
+		 while(scan.hasNext()) {
+		     System.out.println(scan.nextLine());
+		 }
+	}
+}
