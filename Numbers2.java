7cd4875c70f3ca1b10eaec18bf473580494a400d
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-27 15:28:34 +0300

    calling static methods

---
+++ Numbers2.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*; // import Math class
+ 
+class Numbers {
+  public static void main(String[] args) {
+    // Call method by using method name and arguments
+    int smallerNumber = min(3, 10);
+    System.out.println(smallerNumber); // Prints: 3
+  }
+}
