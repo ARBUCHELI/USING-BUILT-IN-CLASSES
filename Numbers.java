2ed1ea45cd663763c4e10052e4cef34847609092
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-27 15:22:06 +0300

    min() method

---
+++ Numbers.java
@@ -0,0 +1,7 @@
+class Numbers {
+  public static void main(String[] args) {
+    // Call method using the Class name, the dot operator, the method name, and arguments
+    int smallerNumber = Math.min(3, 10);
+    System.out.println(smallerNumber); // Prints: 3
+  }
+}
